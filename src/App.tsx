ebf2ce743b392bf0c292633b3237a2fb593dec37
dhruvja <dhruv@iamsizzling.com>
2023-01-31 14:25:41 +0530

    fixed google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import ReactGA from "react-ga";
-const TRACKING_ID = "UA-84290080-2";
-ReactGA.initialize(TRACKING_ID);
+
 
 export const App = () => {
   React.useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
+
+  const TRACKING_ID = "UA-84290080-2";
+  ReactGA.initialize(TRACKING_ID);
 
   return (
     <Router>
